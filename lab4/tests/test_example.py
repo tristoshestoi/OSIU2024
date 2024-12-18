0dbc710b3b41b4c8ead56f01d9f69ca67e0930f5
shuNya19 <kardackow.max@gmail.com>
2024-12-18 10:24:12 +0300

    good ci/cd

---
+++ lab4/tests/test_example.py
@@ -0,0 +1,8 @@
+def test_multiply():
+    assert 10 * 10 == 100, "Multiplication test failed!"
+
+def test_addition():
+    assert 20 + 30 == 50, "Addition test failed!"
+
+def test_subtraction():
+    assert 50 - 20 == 30, "Subtraction test failed!"
